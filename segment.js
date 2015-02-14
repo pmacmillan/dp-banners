62f5e06e05bfea8afb05277194aba65f55cb59a6
Peter MacMillan <peter@macmillan.io>
2015-02-14 12:28:18 -0500

    normalize matrix so that lower two rows can share code

--- segment.js
+++ segment.js
@@ -5,35 +5,32 @@
 // solution by:
 //      Peter MacMillan <peter@macmillan.io>
 //
-//  _    1
-// |_|  234
-// |_|  567
+//  _    null 0x80 null
+// |_|   0x01 0x02 0x04
+// |_|   0x10 0x20 0x40
 //
-//
-//     8765 4321
-// 1   0000 0001
-// 2   0000 0010
-// 4   0000 0100
-// 8   0000 1000
-// 16  0001 0000
-// 32  0010 0000
-// 64  0100 0000
-// 128 1000 0000
-
 //    _  _     _  _  _  _  _
 //  | _| _||_||_ |_   ||_||_|
 //  ||_  _|  | _||_|  ||_| _| 
 
 var charmap = {
-    '0': 0x7B, '1': 0x48, '2': 0x3D, '3': 0x6D, '4': 0x4e,
-    '5': 0x67, '6': 0x77, '7': 0x49, '8': 0x7f, '9': 0x6f
+    '0': 0x80 + 0x01 + 0x04 + 0x10 + 0x20 + 0x40,
+    '1': 0x04 + 0x40,
+    '2': 0x80 + 0x02 + 0x04 + 0x10 + 0x20,
+    '3': 0x80 + 0x02 + 0x04 + 0x20 + 0x40,
+    '4': 0x01 + 0x02 + 0x04 + 0x40,
+    '5': 0x80 + 0x01 + 0x02 + 0x20 + 0x40,
+    '6': 0x80 + 0x01 + 0x02 + 0x10 + 0x20 + 0x40,
+    '7': 0x80 + 0x04 + 0x40,
+    '8': 0x80 + 0x77,
+    '9': 0x80 + 0x01 + 0x02 + 0x04 + 0x20 + 0x40
 };
 
 function bitrow(code) {
     return (
-        ((code & 0x2) ? '|' : ' ') +
-        ((code & 0x4) ? '_' : ' ') +
-        ((code & 0x8) ? '|' : ' '));
+        ((code & 0x1) ? '|' : ' ') +
+        ((code & 0x2) ? '_' : ' ') +
+        ((code & 0x4) ? '|' : ' '));
 }
 
 
@@ -44,12 +41,11 @@ function renderChar(chr) {
         throw new Error('unsupported character: "' + chr + '"');
 
     return [
-        ((code & 0x01) ? ' _ ' : '   '),
+        ((code & 0x80) ? ' _ ' : '   '),
         bitrow(code & 0x0F),
-        bitrow((code & 0xF0) >> 3)
+        bitrow((code & 0x70) >> 4)
     ];
 }
-
 
 function charForCode(code) {
     var key;
@@ -66,9 +62,9 @@ function charForCode(code) {
 function decodebitrow(lines, row) {
     var code = 0;
 
-    if (lines[row][0] === '|') { code += 0x02; }
-    if (lines[row][1] === '_') { code += 0x04; }
-    if (lines[row][2] === '|') { code += 0x08; }
+    if (lines[row][0] === '|') { code += 0x01; }
+    if (lines[row][1] === '_') { code += 0x02; }
+    if (lines[row][2] === '|') { code += 0x04; }
 
     return code;
 }
@@ -77,10 +73,10 @@ function decodeChar(chr) {
     var code = 0,
         lines = chr.split('\n');
 
-    if (lines[0][1] === '_') { code += 0x01; }
+    if (lines[0][1] === '_') { code += 0x80; }
 
     code += decodebitrow(lines, 1);
-    code += (decodebitrow(lines, 2) & 0xf) << 3;
+    code += (decodebitrow(lines, 2) & 0x7) << 4;
 
     return charForCode(code);
 }
